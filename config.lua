4831cd65dbdc771b7be8c03dd227321ac363d4db
Sugus76 <71076411+Sugus76@users.noreply.github.com>
2025-06-09 23:29:21 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,13 @@
 Config = {}
 
 Config.AllowedProps = {
-    [GetHashKey("p_ambpack01x")] = true,
-    [GetHashKey("p_ambpack02x")] = true,
-    [GetHashKey("p_ambpack04x")] = true,
+    [GetHashKey("p_shovel02x")] = true,
+    [GetHashKey("p_ambpack04x")] = false,
 }
+
+Config.BlacklistedAnimations = {
+    ["amb_work@world_human_gravedig@working@male_b@idle_a"] = { "idle_a" },
+    ["amb_work@world_human_farmer_weeding@male_a@idle_c"] = { "idle_g" },
+    ["amb_work@world_human_gravedig@working@male_b@react_look@loop@generic"] = { "react_look_front_loop" }
+}
+
